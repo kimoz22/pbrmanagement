e10102996dba154aca191f4ac4de1614b1a3e452
DESKTOP-MISF2QV\kimox <pala>
2026-05-27 14:35:30 +0300

    filter search approve pending and Cancellation

--- src/components/TicketCancellation.tsx
+++ src/components/TicketCancellation.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useMutation, useQuery } from 'convex/react'
 import { api } from '../../convex/_generated/api'
 import { TicketCancellation } from '../types'
